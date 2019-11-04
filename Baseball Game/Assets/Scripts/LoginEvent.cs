779868b865353b1fc61a5071cf374c9368d40772
KHS0616 <57310672+KHS0616@users.noreply.github.com>
2019-11-04 22:12:22 +0900

    Initial commit

---
+++ Baseball Game/Assets/Scripts/LoginEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginEvent : MonoBehaviour
+{
+    //변수선언
+    //아이디, 비밀번호 입력 필드
+    public InputField inputID, inputPW;
+
+    //유저 정보 클래스
+    public UserInfo user;
+
+    //아이디, 비밀번호 임시 저장 변수
+    string id, pw;
+
+    //테스트용 변수
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = FindObjectOfType<UserInfo>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoginButton()
+    {
+        //입력받은 아이디, 비밀번호 변수 저장 및 할당
+        id = inputID.text;
+        pw = inputPW.text;
+        user.SetUserInfo(id, pw);
+
+        //테스트
+        text.text = id + pw;
+    }
+}
---
+++ Baseball Game/Assets/Scripts/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo : MonoBehaviour
+{
+    //변수 설정
+    //아이디, 비밀번호
+    string id, pw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //삭제 방지
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void SetUserInfo(string id, string pw)
+    {
+        this.id = id;
+        this.pw = pw;
+    }
+}
